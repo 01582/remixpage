b4bd98c4d698944530c19d1604d807b2b8222131
raṅgadevatā <150913511+01582@users.noreply.github.com>
2024-01-31 22:10:13 +0000

    up

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,7 @@
-import { Suspense } from 'react';
 import { defer } from '@vercel/remix';
 import type { LoaderArgs } from '@vercel/remix';
 import { Await, useLoaderData } from '@remix-run/react';
-
-import { Footer } from '~/components/footer';
-import { Region } from '~/components/region';
-import { Illustration } from '~/components/illustration';
+import styles from "./index.module.css"
 import { parseVercelId } from '~/parse-vercel-id';
 
 export const config = { runtime: 'edge' };
@@ -42,39 +38,20 @@ export default function App() {
   return (
     <>
       <main>
-        <Illustration />
-        <div className="meta">
-          <div className="info">
-            <span>Proxy Region</span>
-            <Suspense fallback={<strong>Loading...</strong>}>
-              <Await resolve={proxyRegion}>
-                {(region) => <Region region={region} />}
-              </Await>
-            </Suspense>
-          </div>
-          <div className="info">
-            <span>Compute Region</span>
-            <Suspense fallback={<strong>Loading...</strong>}>
-              <Await resolve={computeRegion}>
-                {(region) => <Region region={region} />}
-              </Await>
-            </Suspense>
-          </div>
-        </div>
-      </main>
+      <div className="bg-[#F5EEE6] ">
+    <div className={styles.main}>
+      <h1 className={styles.heading}>MASTER ANY SUBJECT. <br/> <em>CONQUER ANY TEST.</em> <em> YOUR </em> ACADEMIC SUPERPOWER LIES HERE.
+      </h1>
+     
+    
 
-      <Footer>
-        <p>
-          Generated at {date} <span data-break /> ({isCold ? "cold" : "hot"}) by{" "}
-          <a
-            href="https://vercel.com/docs/concepts/functions/edge-functions"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Vercel Edge Runtime
-          </a>
-        </p>
-      </Footer>
+      
+    </div>
+    </div>
+
+
+
+        </main>
     </>
   );
 }
